cf740ccc45dea43480aa240dac0a19d9e10e6036
Exttasy <94182351+Exttasy1@users.noreply.github.com>
2024-04-13 18:23:12 +0300

    Update hikka_info.py

--- hikka/modules/hikka_info.py
+++ hikka/modules/hikka_info.py
@@ -26,7 +26,7 @@ class HikkaInfoMod(loader.Module):
             ),
             loader.ConfigValue(
                 "custom_button",
-                ["🌘 Support chat", "https://t.me/hikka_talks"],
+                None,
                 lambda: self.strings("_cfg_cst_btn"),
                 validator=loader.validators.Union(
                     loader.validators.Series(fixed_len=2),
@@ -123,7 +123,7 @@ class HikkaInfoMod(loader.Module):
                         "<emoji document_id=5451646226975955576>⌛️</emoji>",
                         "<emoji document_id=5431449001532594346>⚡️</emoji>",
                         "<emoji document_id=5359785904535774578>💼</emoji>",
-                        platform,
+                        "<emoji document_id=5445388468215619925>🕸</emoji><b> ExttasyHost</b>",
                     ),
                 )
             )
